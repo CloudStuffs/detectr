3cb0c05a841205b07413093b3ad1032d513614fd
Faizan Ayubi <indianayubi@gmail.com>
2015-12-09 17:42:28 +0530

    Base methods

--- application/controllers/detectr.php
+++ application/controllers/detectr.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Description of analytics
+ * Description of detectr
  *
  * @author Faizan Ayubi
  */
@@ -11,7 +11,27 @@ use \Curl\Curl;
 
 class Detectr extends Admin {
 
+	public function index() {
+		//echo php code
+	}
+
+	public function create() {
+		
+	}
+
+	public function delete() {
+		$this->noview();
+	}
+
+	public function manage($website_id) {
+		
+	}
+
+	public function all() {
+		
+	}
+
     protected function buildRedirect($url='') {
-        return 'header("Location: '.$url.'")exit;';
+        return 'header("Location: '.$url.'");exit;';
     }
 }
