dabd6b5a14c62c7e57bfb546018ca0a324f3381e
Hemant Mann <hemant.mann121@gmail.com>
2015-12-10 16:40:48 +0530

    Updated views and routes

--- public/routes.php
+++ public/routes.php
@@ -12,6 +12,11 @@ $routes = array(
         "pattern" => "home",
         "controller" => "home",
         "action" => "index"
+    ),
+    array(
+        "pattern" => "login",
+        "controller" => "auth",
+        "action" => "login"
     )
 );
 
